0c269874a929b6d4a3cd85cb9f1f09319a8bb2bc
Francesca Macchiavello <fmacchi@mit.edu>
2020-04-06 17:21:15 -0400

    commit message

---
+++ Desktop/mergeSort.py
@@ -0,0 +1,49 @@
+def mergeSort(arr): 
+    if len(arr) >1: 
+        mid = len(arr)//2 #Finding the mid of the array 
+        L = arr[:mid] # Dividing the array elements  
+        R = arr[mid:] # into 2 halves 
+  
+        print('dividin ', L, R)
+        mergeSort(L) # Sorting the first half 
+        mergeSort(R) # Sorting the second half 
+  
+        i = j = k = 0
+          
+        # Copy data to temp arrays L[] and R[] 
+        print('hello i am sorting now')
+        while i < len(L) and j < len(R): 
+            if L[i] < R[j]: 
+                arr[k] = L[i] 
+                i+=1
+            else: 
+                arr[k] = R[j] 
+                j+=1
+            k+=1
+          
+        # Checking if any element was left 
+        while i < len(L): 
+            arr[k] = L[i] 
+            i+=1
+            k+=1
+          
+        while j < len(R): 
+            arr[k] = R[j] 
+            j+=1
+            k+=1
+        print(arr, 'i am done sorting')
+# Code to print the list 
+def printList(arr): 
+    for i in range(len(arr)):         
+        print(arr[i],end=" ") 
+    print() 
+  
+# driver code to test the above code 
+if __name__ == '__main__': 
+    arr = [12, 13, 11, 7, 6, 4]  
+    print ("Given array is", end="\n")  
+    printList(arr) 
+    mergeSort(arr) 
+    print("Sorted array is: ", end="\n") 
+    printList(arr) 
+  
